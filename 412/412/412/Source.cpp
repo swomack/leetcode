33d268e46b49d3b567891bd23d96db82c26839b1
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-11-10 17:58:43 +0600

    **Commit**

---
+++ 412/412/412/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> fizzBuzz(int n) {
+		vector<string> result(n);
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (i % 3 == 0)
+				result[i - 1] += "Fizz";
+
+			if (i % 5 == 0) 
+			{
+				result[i - 1] += "Buzz";
+				continue;
+			}
+
+
+				
+		}
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<string> fizzbuzz = s.fizzBuzz(15);
+}
---
+++ 419/419/419/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	int countBattleships(vector<vector<char>>& board) 
+	{
+
+	}
+};
+
+int main()
+{
+	Solution s;
+}
