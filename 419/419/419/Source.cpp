92f97ad324c32f2ac97bd6ef2a86d61e7d3e321f
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-11-10 18:13:27 +0600

    **Commit**

--- 419/419/419/Source.cpp
+++ 419/419/419/Source.cpp
@@ -8,7 +8,43 @@ class Solution
 public:
 	int countBattleships(vector<vector<char>>& board) 
 	{
+		int height, width;
 
+		height = board.size();
+		width = board[0].size();
+
+		int total_type = 0;
+
+
+		vector<bool> visited(height * width);
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				int index = i*width + j;
+				visited[index] = false;
+			}
+		}
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				int index = i*width + j;
+
+				if (visited[index])
+					continue;
+
+				visited[index] = true;
+
+				if (board[i][j] == '.')
+					continue;
+
+				
+				// now check which type, might need a hash
+
+			}
+		}
 	}
 };
 
