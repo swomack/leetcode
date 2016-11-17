267c836f3dcb702459f30325cd197cc916e4d850
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-11-17 12:12:10 +0600

    **371**

--- 371/371/371/Source.cpp
+++ 371/371/371/Source.cpp
@@ -7,10 +7,28 @@ class Solution
 public:
 	int getSum(int a, int b) 
 	{
+		int sum = (a == 0 ? b : ((b == 0) ? a : 0));
+		if (sum)
+			return sum;
 
+		sum = a ^ b;
+		int carry = (a & b);
+
+		while (carry)
+		{
+			carry <<= 1;
+			int temp = sum ^ carry;
+			carry &= sum;
+			sum = temp;
+		}
+
+		return sum;
 	}
 };
 
 int main()
 {
+	Solution s;
+	int result = s.getSum(1, 2);
+	getchar();
 }
