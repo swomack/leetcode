646cd2cc1025c726d6875edfdc4e5233f132b3fa
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-11-21 14:43:52 +0600

    **350**

---
+++ 350/350/350/Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+
+
+class Solution
+{
+public:
+	vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
+	{
+
+		if (nums2.size() > nums1.size())
+			return intersect(nums2, nums1);
+
+		vector<int> res;
+
+		if (nums1.size() == 0 || nums2.size() == 0)
+			return res;
+
+		unordered_map<int, int> map;
+
+		for_each(nums2.begin(), nums2.end(), [&map](int element) {
+			map[element] ++;
+		});
+
+		for_each(nums1.begin(), nums1.end(), [&map, &res](int element) {
+			int val = map[element];
+			if (val > 0)
+			{
+				res.push_back(element);
+				map[element]--;
+			}
+
+		});
+
+		return res;
+	}
+};
+
+int main()
+{
+	Solution s;
+
+	vector<int> param1{ 1, 2, 2, 3 };
+	vector<int> param2{ 2, 2 };
+
+	vector<int> result = s.intersection(param1, param2);
+	getchar();
+}
