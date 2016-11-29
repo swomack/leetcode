0ff326252d76eb96c84212315953a2acf32f1006
swomack <sourov0805045@gmail.com>
2016-11-30 01:04:19 +0600

    ** 326 - Log **

---
+++ 326/326/326/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+class Solution 
+{
+public:
+	bool isPowerOfThree(int n) 
+	{
+		if (n <= 0)
+			return false;
+		
+		int log_val = (log10(n)) / log10(3);
+
+		// if 3 ^ log_val = n then it is a power of 3
+		return ((int)pow(3, log_val)) == n;
+	}
+};
+
+
+int main()
+{
+	Solution s;
+	bool res = s.isPowerOfThree(243);
+	res = s.isPowerOfThree(0);
+	res = s.isPowerOfThree(1);
+	res = s.isPowerOfThree(2);
+	res = s.isPowerOfThree(3);
+	res = s.isPowerOfThree(-3);
+	res = s.isPowerOfThree(-1);
+	res = s.isPowerOfThree(27);
+	res = s.isPowerOfThree(26);
+	res = s.isPowerOfThree(81);
+
+	return 0;
+}
