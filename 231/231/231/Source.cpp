f0aee883bff344e9093d9561ca73a826b5b0947e
swomack <sourov0805045@gmail.com>
2016-11-30 00:43:55 +0600

    ** 231 - Fastest **

--- 231/231/231/Source.cpp
+++ 231/231/231/Source.cpp
@@ -7,16 +7,13 @@ class Solution
 public:
 	bool isPowerOfTwo(int n) 
 	{
-		if (n <= 0)
-			return false;
-
-		return __builtin_popcount(n) == 1;
+		return (n > 0 && ((n & (n - 1)) == 0));
 	}
 };
 
 int main()
 {
 	Solution s;
-	bool res = s.isPowerOfTwo(10);
+	bool res = s.isPowerOfTwo(128);
 	return 0;
 }
