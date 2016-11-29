47204a7d983c01ad22dffa3dafcd8af6fe195c39
swomack <sourov0805045@gmail.com>
2016-11-30 01:22:52 +0600

    ** 326 - Fastest - Rank 1 **

--- 326/326/326/Source.cpp
+++ 326/326/326/Source.cpp
@@ -15,11 +15,16 @@ public:
 		if (n % 3) return n == 1;
 
 		if (n % 9) return n == 3;
-		
-		int log_val = (log10(n)) / log10(3);
+		//
+		//int log_val = (log10(n)) / log10(3);
 
-		// if 3 ^ log_val = n then it is a power of 3
-		return ((int)pow(3, log_val)) == n;
+		//// if 3 ^ log_val = n then it is a power of 3
+		//return ((int)pow(3, log_val)) == n;
+
+		// this is the fastest
+		// if the number is power of 3, then it has to divide the maximum 3^power integer = 3 ^ 19 = 1162261467
+		// also 3 ^ 19 will be divisible by only 3^k (its obvious) --- 3 * 3 * 3 .... / 3 * 3 ** 3
+		return 1162261467 % n == 0;
 	}
 };
 
