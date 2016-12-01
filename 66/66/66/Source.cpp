6e28436c98bccfd3781060cfcc9b1f83c8bf7943
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-01 18:16:36 +0600

    Leetcode 66 solution - O(N) memory.

--- 66/66/66/Source.cpp
+++ 66/66/66/Source.cpp
@@ -11,48 +11,17 @@ public:
 	{
 		int n = digits.size();
 
-		if (digits[0] == 9 && digits[n - 1] == 9)
+		while (--n >= 0 && digits[n] == 9)
 		{
-			vector<int> result;
-			--n;
-			while (n >= 0 && digits[n] == 9)
-			{
-				result.push_back(0);
-				n--;
-			}
-
-			result.push_back(digits[n] + 1);
-			n--;
-
-			while (n >= 0)
-			{
-				result.push_back(digits[n]);
-			}
-				
-
-			reverse(result.begin(), result.end());
-			return result;
+			digits[n] = 0;
 		}
+
+		if (n < 0)
+			digits.insert(digits.begin(), 1);
 		else
-		{
-			vector<int> result(n);
-			n--;
-			while (n >= 0 && digits[n] == 9)
-			{
-				result[n] = 0;
-				--n;
-			}
+			digits[n] += 1;
 
-			result[n] = digits[n] + 1;
-			n--;
-			while (n >= 0)
-			{
-				result[n] = digits[n];
-				n--;
-			}
-			
-			return result;
-		}
+		return digits;
 		
 	}
 };
