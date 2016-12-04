028287bcbc505beb3d8915d28339fbbeb6269f0d
swomack <sourov0805045@gmail.com>
2016-12-05 01:31:03 +0600

    Leetcode 111 solution.

--- 111/111/111/Source.cpp
+++ 111/111/111/Source.cpp
@@ -1,14 +1,43 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
 
- struct TreeNode {
+using namespace std;
+
+ struct TreeNode 
+ {
      int val;
      TreeNode *left;
      TreeNode *right;
      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  };
 
-class Solution {
+class Solution 
+{
 public:
-	int minDepth(TreeNode* root) {
+	int minDepth(TreeNode* root) 
+	{
+		if (root == NULL)
+			return 0;
 
+		if (root->left == NULL && root->right == NULL)
+			return 1;
+
+		int left = INT_MAX;
+		if (root->left != NULL)
+			left = minDepth(root->left);
+
+		int right = INT_MAX;
+		if (root->right != NULL)
+			right = minDepth(root->right);
+
+		return min(left, right) + 1;
 	}
 };
+
+int main()
+{
+	Solution s;
+	
+	return 0;
+}
