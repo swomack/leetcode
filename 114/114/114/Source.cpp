b645befef3f09196f48057a419320f0c6c9f8bc9
swomack <sourov0805045@gmail.com>
2016-12-06 00:27:09 +0600

    Leetcode 114 solution.

--- 114/114/114/Source.cpp
+++ 114/114/114/Source.cpp
@@ -13,13 +13,39 @@ struct TreeNode
 class Solution 
 {
 public:
+
+	TreeNode* find_leaf(TreeNode* node)
+	{
+		if (node == NULL)
+			return NULL;
+
+		if (node->left == NULL && node->right == NULL)
+			return node;
+
+		return find_leaf(node->right);
+	}
+
 	void flatten(TreeNode* root) 
 	{
+		if (root == NULL)
+			return;
 
+		flatten(root->left);
+		flatten(root->right);
+
+		TreeNode* left_leaf = find_leaf(root->left);
+
+		if (left_leaf)
+		{
+			left_leaf->right = root->right;
+			root->right = root->left;
+			root->left = NULL;
+		}
 	}
 };
 
 int main()
 {
 	Solution s;
+	return 0;
 }
