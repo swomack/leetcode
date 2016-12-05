4e2ca4bce4fb3dbe675e5c7cb600945dace77f17
swomack <sourov0805045@gmail.com>
2016-12-06 00:45:07 +0600

    Leetcode 98 solution. **Inorder traversal and sort check (wont work if it has two same node) **Not accepted

---
+++ 98/98/98/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution 
+{
+public:
+
+	void inOrderTraverse(TreeNode* node, vector<int>& result)
+	{
+		if (node == NULL)
+			return;
+
+		inOrderTraverse(node->left, result);
+		result.push_back(node->val);
+		inOrderTraverse(node->right, result);
+	}
+
+	bool isValidBST(TreeNode* root) 
+	{
+		if (root == NULL)
+			return true;
+
+		vector<int> in_order;
+		inOrderTraverse(root, in_order);
+
+		for (int i = 0; i < in_order.size() - 1; i++)
+		{
+			if (in_order[i + 1] < in_order[i])
+				return false;
+		}
+
+		return true;
+	}
+};
+
+
+int main()
+{
+	Solution s;
+	return 0;
+}
