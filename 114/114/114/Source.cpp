852c186e236ed05906fc955b0b5c0e64fd3c4845
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-05 14:06:02 +0600

    114 Continued

---
+++ 114/114/114/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution 
+{
+public:
+	void flatten(TreeNode* root) 
+	{
+
+	}
+};
+
+int main()
+{
+	Solution s;
+}
