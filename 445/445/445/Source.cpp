64da6567ea8342ea7f19b782ee33981ba101ad78
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-05 16:55:58 +0600

    Leetcode 445 solution. *reverse Linklist

---
+++ 445/445/445/Source.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+
+	ListNode* reverse(ListNode* prev, ListNode* next)
+	{
+		if (next == NULL)
+			return prev;
+
+		ListNode* temp = reverse(next, next->next);
+
+		next->next = prev;
+		prev->next = NULL;
+
+		return temp;
+
+	}
+
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+	{
+		if (l2 == NULL)
+			return l1;
+
+		if (l1 == NULL)
+			return l2;
+
+		l1 = reverse(l1, l1->next);
+		l2 = reverse(l2, l2->next);
+
+		int carry = 0;
+		int sum = l1->val + l2->val;
+		carry = sum / 10;
+		sum %= 10;
+		ListNode* res = new ListNode(sum);
+		ListNode* temp = res;
+		l1 = l1->next;
+		l2 = l2->next;
+
+		while (l1 && l2)
+		{
+			int sum = l1->val + l2->val + carry;
+			carry = sum / 10;
+			sum %= 10;
+
+			temp->next = new ListNode(sum);
+			l1 = l1->next;
+			l2 = l2->next;
+			temp = temp->next;
+		}
+
+		while (l1)
+		{
+			int sum = l1->val + carry;
+			carry = sum / 10;
+			sum %= 10;
+
+			temp->next = new ListNode(sum);
+			l1 = l1->next;
+			temp = temp->next;
+		}
+
+		while (l2)
+		{
+			int sum = l2->val + carry;
+			carry = sum / 10;
+			sum %= 10;
+
+			temp->next = new ListNode(sum);
+			l2 = l2->next;
+			temp = temp->next;
+		}
+
+		if(carry)
+			temp->next = new ListNode(1);
+
+		return reverse(res, res->next);
+	
+	}
+};
+
+int main()
+{
+	Solution s;
+
+	ListNode* l1 = new ListNode(1);
+	l1->next = new ListNode(2);
+	l1->next->next = new ListNode(3);
+	l1->next->next->next = new ListNode(4);
+
+	ListNode* l2 = new ListNode(5);
+	l2->next = new ListNode(6);
+	l2->next->next = new ListNode(7);
+	l2->next->next->next = new ListNode(8);
+
+	ListNode* result = s.addTwoNumbers(l1, l2);
+
+	return 0;
+}
