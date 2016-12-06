6a1e459273dfeeeb33400a6975bd24ab867dbc76
swomack <sourov0805045@gmail.com>
2016-12-06 23:45:22 +0600

    leetcode 331 solution.

--- 331/331/331/Source.cpp
+++ 331/331/331/Source.cpp
@@ -12,44 +12,47 @@ class Solution
 {
 public:
 
-	void split(const string &s, char delim, stack<string> &elems) 
-	{
-		stringstream ss;
-		ss.str(s);
-		string item;
-		while (getline(ss, item, delim)) 
-		{
-			elems.push(item);
-		}
-	}
-
-
-	stack<string> split(const string &s, char delim) 
-	{
-		stack<string> elems;
-		split(s, delim, elems);
-		return elems;
-	}
-
 	bool isValidSerialization(string preorder) 
 	{
-		stack<string> nodes = split(preorder, ',');
-		
-		if (nodes.size() == 0)
-			return false;
-
-		while (!nodes.empty())
+		stack<string> nodes;
+		stringstream ss;
+		ss.str(preorder);
+		string item;
+		if(getline(ss, item, ','))
+			nodes.push(item);
+		while (getline(ss, item, ','))
 		{
-			if (nodes.top() != "#")
+			if (item != "#")
+			{
+				nodes.push(item);
+			}
+			else if(!nodes.empty())
+			{
+				if (nodes.top() != "#")
+					nodes.push(item);
+				else
+				{
+					while (!nodes.empty() && nodes.top() == "#")
+					{
+						nodes.pop();
+
+						if (nodes.empty() || nodes.top() == "#")
+							return false;
+
+						nodes.pop();
+					}
+
+					nodes.push(item);
+				}
+				
+			}
+			else
+			{
 				return false;
-
-			nodes.pop();
-
-			if (nodes.empty() || nodes.top() != "#")
-				return false;
-
-			nodes.pop();
+			}
 		}
+
+		return nodes.size() == 1 && nodes.top() == "#";
 	}
 };
 
