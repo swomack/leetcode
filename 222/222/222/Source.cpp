a0f6e2bfa7c14a1be924cc399cfe6a789fe20994
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-07 19:03:22 +0600

    222 continued.

---
+++ 222/222/222/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution 
+{
+public:
+
+	TreeNode* 
+
+	int countNodes(TreeNode* root) 
+	{
+		if (root == NULL)
+			return 0;
+
+		while (true)
+		{
+
+		}
+
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
