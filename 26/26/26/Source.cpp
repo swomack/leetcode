5134b475ff3c3ed1937f3bbee0e4b46b03ec8003
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-08 16:55:53 +0600

    **26 Continued**

--- 26/26/26/Source.cpp
+++ 26/26/26/Source.cpp
@@ -9,23 +9,93 @@ class Solution
 public:
 	int removeDuplicates(vector<int>& nums) 
 	{
-		int length = nums.size();
-		int k = length;
 
-		for (int i = 0; i < k - 1; i++)
+		if (nums.size() <= 0)
+			return 0;
+
+		if (nums.size() == 1)
+			return 1;
+
+		vector<int> temp;
+		vector<int> dup;
+		int length = nums.size();
+
+		int index = 0;
+		while (index < length - 1)
 		{
-			if (nums[i] == nums[i + 1])
-				length--;
+			if (nums[index] == nums[index + 1])
+			{
+				int val = nums[index];
+				temp.push_back(val);
+
+				index++;
+
+				while (index < length && nums[index] == nums[index - 1])
+				{
+					dup.push_back(nums[index]);
+					index++;
+				}
+			}
+			else
+			{
+				int val = nums[index];
+				temp.push_back(val);
+				index++;
+			}
 		}
 
-		return length;
+		index = 0;
+
+		for (int i = 0; i < temp.size(); i++)
+			nums[index++] = temp[i];
+
+		for (int i = 0; i < dup.size(); i++)
+			nums[index++] = dup[i];
+
+		return temp.size();
+
+		/*int length = nums.size();
+		int head = 1;
+		int tail = 1;
+
+		while(head < length)
+		{
+			if (nums[head] != nums[head - 1])
+			{
+				int temp = nums[head];
+				nums[head] = nums[tail];
+				nums[tail] = temp;
+
+				head++;
+				tail++;
+			}
+			else
+			{
+				int value = nums[head];
+				while (head < length && nums[head] == value)
+					head++;
+
+				if (head != length)
+				{
+					int temp = nums[head];
+					nums[head] = nums[tail];
+					nums[tail] = temp;
+
+					head++;
+					tail++;
+				}
+				
+			}
+		}
+
+		return tail;*/
 	}
 };
 
 int main()
 {
 	Solution s;
-	vector<int> v{ };
+	vector<int> v{1, 1, 2, 2};
 	int length = s.removeDuplicates(v);
 	return 0;
 }
