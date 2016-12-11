8628ce37f864cf2af5e903f4fc6ca30a8705f688
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-11 17:18:11 +0600

    Leetcode 215 solution. ** Using incomplete quicksort.

--- 215/215/215/Source.cpp
+++ 215/215/215/Source.cpp
@@ -4,27 +4,61 @@
 
 using namespace std;
 
-struct cmpStruct 
-{
-	bool operator() (int const & lhs, int const & rhs) const
-	{
-		return lhs > rhs;
-	}
-};
-
 class Solution 
 {
 public:
+
+	void swap(int *a, int *b)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+
+	int partition(vector<int>& nums, int i, int j)
+	{
+		int lower = i - 1;
+
+		for (; i < j; i++)
+		{
+			if (nums[i] > nums[j])
+			{
+				lower++;
+				swap(&nums[lower], &nums[i]);
+			}
+		}
+
+		lower++;
+		swap(&nums[lower], &nums[j]);
+
+		return lower;
+	}
+
+	void findKthLargestQuickSort(vector<int>& nums, int i, int j, int k)
+	{
+		int pivot_pos = partition(nums, i, j);
+
+		if (pivot_pos == k)
+			return;
+
+		if (k < pivot_pos)
+			findKthLargestQuickSort(nums, i, pivot_pos - 1, k);
+		else
+			findKthLargestQuickSort(nums, pivot_pos + 1, j, k);
+	}
+
 	int findKthLargest(vector<int>& nums, int k) 
 	{
-		sort(nums.begin(), nums.end(), cmpStruct());
+		findKthLargestQuickSort(nums, 0, nums.size() - 1, --k);
 
-		return nums[k - 1];
+		return nums[k];
 	}
 };
 
 int main()
 {
 	Solution s;
+	vector<int> param{ 2,1,3 };
+	int largest = s.findKthLargest(param, 2);
 	return 0;
 }
