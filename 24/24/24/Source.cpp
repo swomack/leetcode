f5d22698587a13bb6333b00360381a544425a167
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-11 20:17:44 +0600

    **24 continued**

---
+++ 24/24/24/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution 
+{
+public:
+	ListNode* swapPairs(ListNode* head) 
+	{
+
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
