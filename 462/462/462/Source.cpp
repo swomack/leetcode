f184746d3b4ef62ec1d9d9b8be596b6ab1ca98a7
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-12 18:52:01 +0600

    Leetcode 462 solution. * O(n) - QuickSelect - Medians of Medians

--- 462/462/462/Source.cpp
+++ 462/462/462/Source.cpp
@@ -8,14 +8,55 @@ using namespace std;
 class Solution 
 {
 public:
+
+	void swap(int *a, int *b)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+
+	int partition(vector<int>& nums, int i, int j, int pivot_index)
+	{
+		swap(&nums[j], &nums[pivot_index]);
+
+		int start = i;
+
+		for (; i < j; i++)
+		{
+			if (nums[i] < nums[j])
+			{
+				swap(&nums[start], &nums[i]);
+				start++;
+			}
+		}
+
+		swap(&nums[start], &nums[j]);
+
+		return start;
+	}
+
+	int quickSelect(vector<int>& nums, int i, int j, int k)
+	{
+		if (i == j)
+			return nums[i];
+
+		int pivot_index = i + floor(rand() % (j - i + 1));
+		pivot_index = partition(nums, i, j, pivot_index);
+
+		if (pivot_index == k)
+			return nums[pivot_index];
+
+		if (pivot_index < k)
+			return quickSelect(nums, pivot_index + 1, j, k);
+
+		return quickSelect(nums, i, pivot_index - 1, k);
+	}
+
 	int minMoves2(vector<int>& nums) 
 	{
-		sort(nums.begin(), nums.end());
-
-		int median = nums[nums.size() / 2];
-
+		int median = quickSelect(nums, 0, nums.size() - 1, nums.size() / 2);
 		int count = 0;
-
 		for_each(nums.begin(), nums.end(), [&count, &median](int element) {
 			count += abs(element - median);
 		});
