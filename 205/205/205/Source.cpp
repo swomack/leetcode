ed1cade876a75a34752167029ca0da94d5c4c7b4
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-12 16:28:42 +0600

    Leetcode 205 solution. * Using map. O(n)

--- 205/205/205/Source.cpp
+++ 205/205/205/Source.cpp
@@ -5,70 +5,31 @@
 
 using namespace std;
 
-struct comp
-{
-	bool operator () (vector<int>& lhs, vector<int>& rhs)
-	{
-		if (lhs.size() == 0 && rhs.size() == 0)
-			return true;
-		else if (lhs.size() > rhs.size())
-			return true;
-		else if (lhs[0] > rhs[0])
-			return true;
-
-		return false;
-	}
-};
-
 class Solution 
 {
 public:
 
-	bool isEqual(vector<int>& a, vector<int>& b)
-	{
-		if (a.size() != b.size())
-			return false;
-
-		for (int i = 0; i < a.size(); i++)
-		{
-			if (a[i] != b[i])
-				return false;
-		}
-
-		return true;
-	}
-
 	bool isIsomorphic(string s, string t) 
 	{
-		vector<vector<int>> count1(256);
-		vector<vector<int>> count2(256);
-
-		int index = 0;
-
-		for_each(s.begin(), s.end(), [&count1, &index](char element) {
-			count1[element].push_back(index++);
-		});
-
-		index = 0;
-		for_each(t.begin(), t.end(), [&count2, &index](char element) {
-			count2[element].push_back(index++);
-		});
-
-		sort(count1.begin(), count1.end());
-		sort(count2.begin(), count2.end());
-
 		bool result = true;
 
-		for (int i = 255; i >= 0; i--)
+		unordered_map<char, char> map_s;
+		unordered_map<char, char> map_t;
+
+		for (int i = 0; i < s.length(); i++)
 		{
-			if (count1[i].size() == 0 && count2[i].size() == 0)
+			int val_t = map_t[t[i]];
+			int val_s = map_s[s[i]];
+			if (val_t == 0 && val_s == 0)
+			{
+				map_t[t[i]] = s[i];
+				map_s[s[i]] = t[i];
+			}
+			else if(val_t != s[i] || val_s != t[i])
+			{
+				result = false;
 				break;
-
-			if (isEqual(count1[i], count2[i]))
-				continue;
-
-			result = false;
-			break;
+			}
 		}
 
 		return result;
