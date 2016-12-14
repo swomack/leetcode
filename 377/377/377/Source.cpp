81b59ea054de61866793630a11c9a59ce40676e6
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-14 19:52:34 +0600

    Leetcode 377 solution. ** DP (Coin change)

--- 377/377/377/Source.cpp
+++ 377/377/377/Source.cpp
@@ -8,41 +8,24 @@ using namespace std;
 class Solution
 {
 public:
-	unordered_map<int, int> count;
+	
 	int combinationSum4(vector<int>& nums, int target)
 	{
-		count.clear();
-		return combinationSumHelper(nums, target);
-	}
+		vector<int> count(target + 1);
 
-	int combinationSumHelper(vector<int>& nums, int target)
-	{
-		int results = 0;
-
-		if (target < 0)
-			return results;
-
-		if (count[target])
-			return count[target];
-
-		for (int i = 0; i < nums.size(); i++)
+		count[0] = 1;
+		
+		for (int i = 1; i <= target; i++)
 		{
-			if (target - nums[i] == 0)
+			for (int j = 0; j < nums.size(); j++)
 			{
-				results++;
+				if (i - nums[j] >= 0)
+					count[i] += count[i - nums[j]];
 			}
-			else
-			{
-				results += combinationSum4(nums, target - nums[i]);
-			}
-
 		}
 
-		count[target] = results;
-
-		return results;
+		return count[target];
 	}
-
 };
 
 int main()
