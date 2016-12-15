022af4949a82ef0999fe7ed1526c8f7c95917c18
swomack <sourov0805045@gmail.com>
2016-12-15 09:21:54 +0600

    Leetcode 319 solution. * Perfect square

---
+++ 319/319/319/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+class Solution 
+{
+public:
+	int bulbSwitch(int n) 
+	{
+		return sqrt(n);
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
