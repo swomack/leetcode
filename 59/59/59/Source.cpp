4b878f3aa6505f41b7a9ce24377e96c09f160fb2
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-20 14:02:01 +0600

    Leetcode 59 solution. * 2 layers

---
+++ 59/59/59/Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution 
+{
+public:
+
+	void fillBottomLeftLayer(int row_start, int row_end, int col_start, int col_end, int numbering_start, vector<vector<int>>& result)
+	{
+		if (row_start > row_end || col_start > col_end)
+			return;
+
+		for (int i = col_end; i > col_start; i--)
+			result[row_end][i] = numbering_start++;
+
+		for (int i = row_end; i >= row_start; i--)
+			result[i][col_start] = numbering_start++;
+
+		fillTopRightLayer(row_start, row_end - 1, col_start + 1, col_end, numbering_start, result);
+	}
+
+	void fillTopRightLayer(int row_start, int row_end, int col_start, int col_end, int numbering_start, vector<vector<int>>& result)
+	{
+		if (row_start > row_end || col_start > col_end)
+			return;
+
+		for (int i = col_start; i < col_end; i++)
+			result[row_start][i] = numbering_start++;
+
+		for (int i = row_start; i <= row_end; i++)
+			result[i][col_end] = numbering_start++;
+
+		fillBottomLeftLayer(row_start + 1, row_end, col_start, col_end - 1, numbering_start, result);
+	}
+
+	vector<vector<int>> generateMatrix(int n) 
+	{
+		vector<vector<int>> matrix(n);
+
+		for (int i = 0; i < n; i++)
+			matrix[i].resize(n);
+
+		if (n == 0)
+			return matrix;
+
+		fillTopRightLayer(0, n - 1, 0, n - 1, 1, matrix);
+
+		return matrix;
+	}
+};
+
+int main()
+{
+	Solution s;
+
+	vector<vector<int>> res = s.generateMatrix(5);
+
+	return 0;
+}
