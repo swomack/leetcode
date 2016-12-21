21a34b35f0d616438747a61a367a7c57ac7b5efe
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-21 09:05:52 +0600

    Leetcode 477 solution.

--- 477/477/477/Source.cpp
+++ 477/477/477/Source.cpp
@@ -6,22 +6,25 @@ using namespace std;
 
 class Solution {
 public:
-
-	int pop_count(int n)
-	{
-		return __builtin_popcount(n);
-	}
-
 	int totalHammingDistance(vector<int>& nums) 
 	{
 		int count = 0;
 
-		for (int i = 0; i < nums.size() - 1; i++)
+		int array_size = sizeof(int) * 8;
+		vector<int> ones(array_size);
+
+		int one = 1;
+
+		for (int i = 0; i < array_size; i++)
 		{
-			for (int j = i + 1; j < nums.size(); j++)
+			int mask = (one << (31 - i));
+			for (int j = 0; j < nums.size(); j++)
 			{
-				count += pop_count(nums[i] ^ nums[j]);
+				if (nums[j] & mask)
+					ones[i]++;
 			}
+
+			count += (ones[i] * (nums.size() - ones[i]));
 		}
 
 		return count;
@@ -31,5 +34,10 @@ public:
 int main()
 {
 	Solution s;
+
+	vector<int> param{ 1,2,3,4 };
+
+	int dist = s.totalHammingDistance(param);
+
 	return 0;
 }
