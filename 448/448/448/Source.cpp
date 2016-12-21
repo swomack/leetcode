3cb654648532e513a433cad6ccb53e08a911120f
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-21 10:09:21 +0600

    Leetcode 448 solution. * Improved algo in O(1) memory

--- 448/448/448/Source.cpp
+++ 448/448/448/Source.cpp
@@ -4,41 +4,23 @@
 
 using namespace std;
 
-class Solution 
-{
+class Solution {
 public:
-
-	void swap(int* a, int* b)
-	{
-		int temp = *a;
-		*a = *b;
-		*b = temp;
-	}
-
 	vector<int> findDisappearedNumbers(vector<int>& nums) 
 	{
-		vector<int> res;
-
 		for (int i = 0; i < nums.size(); i++)
 		{
-			while (true)
-			{
-				if (nums[i] == nums[nums[i] - 1])
-					break;
-				swap(&nums[i], &nums[nums[i] - 1]);
-			}
-				
+			int index = abs(nums[i]) - 1; 
+			nums[index] = nums[index] > 0 ? -nums[index] : nums[index]; // negate to indicate that this position has appropriate item
 		}
 
-	
+		vector<int> res;
 		for (int i = 0; i < nums.size(); i++)
 		{
-			if (nums[i] != i + 1)
+			if (nums[i] > 0) 
 				res.push_back(i + 1);
 		}
-
 		return res;
-
 	}
 };
 
