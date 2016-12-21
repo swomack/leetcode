4cf765ce4b3280b4753589d6fc6c8395a14bd28d
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-21 10:23:19 +0600

    Leetcode 442 solution. * Runtime: O(n) * Memory: O(1)

--- 442/442/442/Source.cpp
+++ 442/442/442/Source.cpp
@@ -9,16 +9,17 @@ public:
 	vector<int> findDuplicates(vector<int>& nums) 
 	{
 		int length = nums.size();
-		vector<int> map(length);
-		for (int i = 0; i < length; i++)
-			map[nums[i] - 1] ++;
-
 		vector<int> res;
-
+		
 		for (int i = 0; i < length; i++)
 		{
-			if (map[i] == 2)
-				res.push_back(i + 1);
+			int index = abs(nums[i]) - 1;
+
+			if (nums[index] < 0) // already encountered
+				res.push_back(abs(nums[i]));
+			else         // mark that it has been encountered
+				nums[index] *= -1;
+
 		}
 
 		return res;
