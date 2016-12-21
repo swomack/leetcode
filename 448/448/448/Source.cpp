62b058c27dfb07afc64f729ad407334b8777347c
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-21 09:51:01 +0600

    Leetcode 448 solution. * O(1) memory

--- 448/448/448/Source.cpp
+++ 448/448/448/Source.cpp
@@ -7,18 +7,33 @@ using namespace std;
 class Solution 
 {
 public:
+
+	void swap(int* a, int* b)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+
 	vector<int> findDisappearedNumbers(vector<int>& nums) 
 	{
-		vector<int> map(nums.size());
-
-		for (int i = 0; i < nums.size(); i++)
-			map[nums[i] - 1] = nums[i];
-
 		vector<int> res;
 
-		for (int i = 0; i < map.size(); i++)
+		for (int i = 0; i < nums.size(); i++)
 		{
-			if (map[i] == 0)
+			while (true)
+			{
+				if (nums[i] == nums[nums[i] - 1])
+					break;
+				swap(&nums[i], &nums[nums[i] - 1]);
+			}
+				
+		}
+
+	
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] != i + 1)
 				res.push_back(i + 1);
 		}
 
@@ -30,5 +45,7 @@ public:
 int main()
 {
 	Solution s;
+	vector<int> param{ 4,3,2,7,8,2,3,1 };
+	vector<int> res = s.findDisappearedNumbers(param);
 	return 0;
 }
