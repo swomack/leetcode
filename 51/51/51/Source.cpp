0252bdfb0efcc60cb0445d08699916fec5fd33cd
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-22 14:56:24 +0600

    Leetcode 51 solution. * Backtracking. (Little improved)

--- 51/51/51/Source.cpp
+++ 51/51/51/Source.cpp
@@ -9,40 +9,19 @@ class Solution
 {
 public:
 
-	int get_row(vector<string>& matrix, int col, int N)
+	bool is_safe(vector<string>& matrix, int N, vector<int>& rows)
 	{
-		for (int i = 0; i < N; i++)
-		{
-			if (matrix[i][col] == 'Q')
-				return i;
-		}
-
-		return -1;
-	}
-
-	bool is_safe(vector<string>& matrix, int N)
-	{
-		vector<int> positions;
-
-		for (int i = 0; i < N; i++)
-		{
-			int row = get_row(matrix, i, N);
-
-			if (row >= 0)
-				positions.push_back(row);
-		}
-
-		if (positions.size() <= 1)
+		if (rows.size() == 1)
 			return true;
 
-		for (int i = 1; i < positions.size(); i++)
+		for (int i = 1; i < rows.size(); i++)
 		{
 			for (int j = 0; j < i; j++)
 			{
-				if (positions[i] == positions[j])
+				if (rows[i] == rows[j])
 					return false;
 
-				if (positions[i] + (j - i) == positions[j] || positions[i] + (i - j) == positions[j])
+				if (rows[i] + (j - i) == rows[j] || rows[i] + (i - j) == rows[j])
 					return false;
 			}
 		}
@@ -51,7 +30,7 @@ public:
 	}
 
 
-	void NQueen(vector<string>& matrix, int column, int N, vector<vector<string>>& solutions)
+	void NQueen(vector<string>& matrix, int column, int N, vector<vector<string>>& solutions, vector<int>& rows)
 	{
 		if (N < 4 && N > 1)
 			return;
@@ -68,11 +47,13 @@ public:
 		for (int i = 0; i < N; i++)
 		{
 			matrix[i][column] = 'Q';
+			rows.push_back(i);
 
-			if (is_safe(matrix, N))
-				NQueen(matrix, column + 1, N, solutions);
+			if (is_safe(matrix, N, rows))
+				NQueen(matrix, column + 1, N, solutions, rows);
 
 			matrix[i][column] = '.';
+			rows.pop_back();
 		}
 	}
 
@@ -92,7 +73,8 @@ public:
 			matrix.push_back(s);
 		}
 
-		NQueen(matrix, 0, n, solutions);
+		vector<int> rows;
+		NQueen(matrix, 0, n, solutions, rows);
 
 
 
