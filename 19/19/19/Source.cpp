1f3b1767e6387afdd9a2c33ac8e64d554ef65076
swomack <sourov0805045@gmail.com>
2016-12-26 10:35:45 +0600

    ** 19 Continued **

---
+++ 19/19/19/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+
+	ListNode* removeNthFromEnd(ListNode* head, int n) 
+	{
+
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
