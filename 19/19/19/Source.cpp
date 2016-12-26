6460dca170600861fa90363c124f38dd718cb1d8
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-26 12:39:51 +0600

    Leetcode 19 solution. * Single pass recursion.

--- 19/19/19/Source.cpp
+++ 19/19/19/Source.cpp
@@ -12,9 +12,33 @@ struct ListNode
 class Solution {
 public:
 
+	ListNode* removingNode(ListNode* node, int& n)
+	{
+		if (node->next == NULL)
+			return NULL;
+
+		ListNode* ret = removingNode(node->next, n);
+
+		n--;
+
+		if (n == 0)
+			return node;
+
+		return ret;
+
+	}
+
 	ListNode* removeNthFromEnd(ListNode* head, int n) 
 	{
+		ListNode* temp = head;
+		ListNode* rem = removingNode(temp, n);
 
+		if (rem == NULL)
+			head = head->next;
+		else
+			rem->next = rem->next->next;
+
+		return head;
 	}
 };
 
