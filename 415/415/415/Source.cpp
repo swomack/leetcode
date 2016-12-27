bd6ed1514b061df7c6b22ce5eaccd01a8b74f686
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-27 15:32:12 +0600

    Leetcode 415 solution.

--- 415/415/415/Source.cpp
+++ 415/415/415/Source.cpp
@@ -18,39 +18,36 @@ public:
 	string addStrings(string num1, string num2) 
 	{
 		string res;
-		reverse(num1.rbegin(), num1.rend());
-		reverse(num2.rbegin(), num2.rend());
-
+	
 		if (num2.length() > num1.length())
 			swap(&num1, &num2);
 
-		int index = 0;
+		int index2 = num2.length() - 1;
+		int index1 = num1.length() - 1;
 		int carry = 0;
 		int sub = 2 * '0';
-		while (index < num2.length())
+		while (index2 >= 0)
 		{
-			int res_digit = num1[index] + num2[index] - sub + carry;
+			int res_digit = num1[index1--] + num2[index2--] - sub + carry;
 			carry = res_digit / 10;
 			res_digit %= 10;
-
 			res.push_back(res_digit + '0');
-			index++;
 		}
 
 		sub = '0';
 
-		while (index < num1.length())
+		while (index1 >= 0)
 		{
-			int res_digit = num1[index] - sub + carry;
+			int res_digit = num1[index1--] - sub + carry;
 			carry = res_digit / 10;
 			res_digit %= 10;
-
 			res.push_back(res_digit + '0');
-			index++;
 		}
 
 		if (carry)
+		{
 			res.push_back('1');
+		}
 
 		reverse(res.rbegin(), res.rend());
 
