77d2ac348e9ba01ee8a92d1a011a4ef1028c8d76
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-29 13:00:30 +0600

    Leetcode 148 solution. * Quicksort (TLE)

--- 148/148/148/Source.cpp
+++ 148/148/148/Source.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -13,33 +11,102 @@ struct ListNode {
 class Solution 
 {
 public:
+	int length;
+	ListNode* list_head;
 	ListNode* sortList(ListNode* head) 
 	{
-		vector<int> sorted_list;
+		if (head == NULL || head->next == NULL)
+			return head;
+
+		list_head = head;
 		ListNode* temp = head;
-
-		while (temp)
+		length = 0;
+		while (temp->next)
 		{
-			sorted_list.push_back(temp->val);
+			length++;
 			temp = temp->next;
 		}
 
-		sort(sorted_list.begin(), sorted_list.end());
+		quickSort(head, temp, 0, length);
 
-		temp = head;
+		return list_head;
+	}
 
-		for (int i = 0; i < sorted_list.size(); i++)
+	void quickSort(ListNode* start, ListNode* end, int start_index, int end_index)
+	{
+		if (start_index >= end_index)
+			return;
+
+		int pivot_offset = floor(rand() % (end_index - start_index + 1));
+		pivot_offset = partition(start, end, pivot_offset);
+
+		if (pivot_offset == 0)
 		{
-			temp->val = sorted_list[i];
+			quickSort(start->next, end, start_index + 1, end_index);
+		}
+		else
+		{
+			ListNode* pivot = start;
+			int midpoint = pivot_offset;
+			while (pivot_offset - 1)
+			{
+				pivot_offset--;
+				pivot = pivot->next;
+			}
+
+			quickSort(start, pivot, start_index, start_index + midpoint - 1);
+
+			if(start_index + midpoint + 1 <= end_index)
+				quickSort(pivot->next->next, end, start_index + midpoint + 1, end_index);
+		}
+	}
+
+	int partition(ListNode* start, ListNode* end, int pivot_index)
+	{
+		ListNode* pivot_node = start;
+		while (pivot_index--)
+		{
+			pivot_node = pivot_node->next;
+		}
+
+		swap(end, pivot_node);
+
+		ListNode* temp = start;
+
+		int less_equal = 0;
+		while(temp != end)
+		{
+			if (temp->val <= end->val)
+			{
+				swap(start, temp);
+				start = start->next;
+				less_equal++;
+			}
+
 			temp = temp->next;
 		}
 
-		return head;
+		swap(start, end);
+
+		return less_equal;
+	}
+
+	void swap(ListNode* a, ListNode* b)
+	{
+		int temp = a->val;
+		a->val = b->val;
+		b->val = temp;
 	}
 };
 
 int main()
 {
 	Solution s;
+	ListNode* head = new ListNode(3);
+	head->next = new ListNode(2);
+	head->next->next = new ListNode(1);
+
+	ListNode* res = s.sortList(head);
+
 	return 0;
 }
