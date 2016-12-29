0f248867ce10c7554868708728992d5716a257b9
swomack <sourov0805045@gmail.com>
2016-12-29 23:48:12 +0600

    Leetcode 63 solution.

--- 63/63/63/Source.cpp
+++ 63/63/63/Source.cpp
@@ -17,16 +17,28 @@ public:
 			results[i].resize(n);
 
 		for (int i = 0; i < m; i++)
+		{
+			if (obstacleGrid[i][0] == 1)
+				break;
+				
 			results[i][0] = 1;
+		}
+			
 
 		for (int i = 0; i < n; i++)
+		{
+			if (obstacleGrid[0][i] == 1)
+				break;
+
 			results[0][i] = 1;
+		}
 
 		for (int i = 1; i < m; i++)
 		{
 			for (int j = 1; j < n; j++)
 			{
-				results[i][j] = results[i - 1][j] + results[i][j - 1];
+				if(obstacleGrid[i][j] != 1)
+					results[i][j] = results[i - 1][j] + results[i][j - 1];
 			}
 		}
 
@@ -37,5 +49,14 @@ public:
 int main()
 {
 	Solution s;
+
+	vector<int> a{ 0,0,0 };
+	vector<int> b{ 0,1,0 };
+	vector<int> c{ 0,0,0 };
+
+	vector<vector<int>> param{ a,b,c };
+
+	int sol = s.uniquePathsWithObstacles(param);
+
 	return 0;
 }
