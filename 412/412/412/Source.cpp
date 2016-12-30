c8e3913026817a871e003afc9eee2aacc97e6635
swomack <sourov0805045@gmail.com>
2016-12-30 12:44:09 +0600

    Leetcode 412 solution.

--- 412/412/412/Source.cpp
+++ 412/412/412/Source.cpp
@@ -11,18 +11,20 @@ public:
 
 		for (int i = 1; i <= n; i++)
 		{
+			if (i % 3 && i % 5)
+			{
+				result[i - 1] = to_string(i);
+				continue;
+			}
+				
 			if (i % 3 == 0)
 				result[i - 1] += "Fizz";
 
-			if (i % 5 == 0) 
-			{
+			if(i % 5 == 0)
 				result[i - 1] += "Buzz";
-				continue;
-			}
-
-
-				
 		}
+
+		return result;
 	}
 };
 
