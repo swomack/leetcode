9642986be2038153a39a24034f0e0561e84256c3
swomack <sourov0805045@gmail.com>
2017-01-03 09:09:27 +0600

    Leetcode 264 solution. * Priority queue * bad solution.

--- 264/264/264/Source.cpp
+++ 264/264/264/Source.cpp
@@ -1,1 +1,41 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	int nthUglyNumber(int n) 
+	{
+		priority_queue<long, vector<long>, greater<long>> queue;
+		queue.push(1);
+		long last_element = -1;
+		while (true)
+		{
+			long top = queue.top();
+			queue.pop();
+			if (last_element == top)
+				continue;
+			last_element = top;
+			n--;
+
+			if (n == 0)
+				break;
+
+			queue.push(2 * top);
+			queue.push(3 * top);
+			queue.push(5 * top);
+		}
+
+		return last_element;
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
