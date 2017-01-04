35e8b47c0eb0fe09eb7f7d4c02b3049b5759f228
swomack <sourov0805045@gmail.com>
2017-01-04 10:09:59 +0600

    *

--- 67/67/67/Source.cpp
+++ 67/67/67/Source.cpp
@@ -7,7 +7,17 @@ using namespace std;
 class Solution {
 public:
 	string addBinary(string a, string b) {
+		int la = a.length();
+		int lb = b.length();
 
+		string res;
+		int carry = 0;
+		while (la-- && lb--)
+		{
+			int digit = (a[la] - '0') ^ (b[lb] - '0') ^ carry;
+			res.push_back('0' + digit);
+			carry = (((a[la] - '0') || (b[lb] - '0')) & carry) || ((a[la] - '0') && (b[lb] - '0'));
+		}
 	}
 };
 
