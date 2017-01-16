8e12d9c0acd75d8de460a917356934cd3e98b1d5
swomack <sourov0805045@gmail.com>
2017-01-16 09:05:38 +0600

    **152 continued**

--- 152/152/152/Source.cpp
+++ 152/152/152/Source.cpp
@@ -18,7 +18,7 @@ public:
 		vector<int> max_pos(nums.size());
 		vector<int> max_neg(nums.size());
 
-		max_pos[0] = nums[0] > 0 ? nums[0] : 1;
+		max_pos[0] = nums[0] > 0 ? nums[0] : INT_MIN;
 		max_neg[0] = nums[0];
 
 
@@ -26,30 +26,60 @@ public:
 		{
 			if (nums[i] > 0)
 			{
-				max_pos[i] = max_pos[i - 1] * nums[i];
-				max_neg[i] = max_neg[i - 1] * nums[i];
-
-				max_prod = max(max_prod, max_pos[i]);
-			}
-			else if (nums[i] < 0)
-			{
-				if (max_neg[i - 1] < 0)
+				if (max_pos[i - 1] == INT_MIN)
 				{
-					max_pos[i] = max_neg[i - 1] * nums[i];
-					max_prod = max(max_prod, max_pos[i]);
+					max_pos[i] = nums[i];
 				}
 				else
 				{
-					max_pos[i] = 1;
+					max_pos[i] = max_pos[i - 1] * nums[i];
 				}
-
-				max_neg[i] = max_neg[i - 1] * nums[i];
+				
+				
+				if (max_neg[i - 1] == INT_MAX)
+				{
+					max_neg[i] = nums[i];
+				}
+				else
+				{
+					max_neg[i] = max_neg[i - 1] * nums[i];
+				}
+			}
+			else if (nums[i] < 0)
+			{
+				if (max_neg[i - 1] == INT_MAX)
+				{
+					max_neg[i] = nums[i];
+				}
+				else if(max_neg[i - 1] < 0)
+				{
+					max_pos[i] = max_neg[i - 1] * nums[i];
+					max_neg[i] = nums[i];
+				}
+				else
+				{
+					max_neg[i] = max_neg[i - 1] * nums[i];
+					max_pos[i] = INT_MIN;
+				}
 			}
 			else
 			{
-				max_pos[i] = max_neg[i] = 1;
+				max_pos[i] = INT_MIN;
+				max_neg[i] = INT_MAX;
 			}
 		}
+
+		max_prod = INT_MIN;
+
+		for (int i = 0; i < max_pos.size(); i++)
+			max_prod = max(max_prod, max_pos[i]);
+
+		for (int i = 0; i < max_neg.size(); i++)
+		{
+			if(max_neg[i] != INT_MAX)
+				max_prod = max(max_prod, max_neg[i]);
+		}
+			
 
 		return max_prod;
 	}
@@ -58,7 +88,7 @@ public:
 int main()
 {
 	Solution s;
-	vector<int> param{ 7,-2,-4 };
+	vector<int> param{ -1,-2,-9,-6 };
 	int res = s.maxProduct(param);
 	return 0;
 }
