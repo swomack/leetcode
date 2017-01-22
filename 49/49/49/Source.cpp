aad64b8dbf415374ac75b781d5d32aad0fd8377c
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-22 19:41:40 +0600

    Leetcode 49 solution. * Improved

--- 49/49/49/Source.cpp
+++ 49/49/49/Source.cpp
@@ -1,58 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution 
 {
 public:
-
-	bool check_equal(vector<int>& a, vector<int>& b)
-	{
-		for (int i = 0; i < 26; i++)
-		{
-			if (a[i] != b[i])
-				return false;
-		}
-
-		return true;
-	}
-
 	vector<vector<string>> groupAnagrams(vector<string>& strs) 
 	{
 		vector<vector<string>> result;
-		vector<pair<int, vector<int>>> group_map;
+		unordered_map<string, int> group_map;
 
 		for (int i = 0; i < strs.size(); i++)
 		{
-			vector<int> map(26);
+			string cpy = strs[i];
+			sort(strs[i].begin(), strs[i].end());
 
-			for (int j = 0; j < strs[i].length(); j++)
-				map[strs[i][j] - 'a']++;
+			int index = group_map[strs[i]];
 
-			bool found = false;
-			for (int j = 0; j < group_map.size(); j++)
+			if (index)
 			{
-				if (group_map[j].first != strs[i].length())
-					continue;
-				bool equal = check_equal(group_map[j].second, map);
-
-				if (equal)
-				{
-					found = true;
-					result[j].push_back(strs[i]);
-					break;
-				}
+				result[index - 1].push_back(cpy);
 			}
-
-			if (!found)
+			else
 			{
-				result.resize(result.size() + 1);
-				result[result.size() - 1].push_back(strs[i]);
-				group_map.push_back(make_pair(strs[i].length(), map));
-			}
-				
+				vector<string> list;
+				list.push_back(cpy);
+				result.push_back(list);
+				group_map[strs[i]] = result.size();
+			}	
 		}
 
 		return result;
