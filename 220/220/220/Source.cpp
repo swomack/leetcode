66f3e15e7137d3b7a67c3a4d3cb0fb1476e59774
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-23 18:50:54 +0600

    *220 continued**

---
+++ 220/220/220/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
