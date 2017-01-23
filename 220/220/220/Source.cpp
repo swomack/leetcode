dfeb3303ab83f349b2d2d360afe8effaf4af0f7a
swomack <sourov0805045@gmail.com>
2017-01-24 01:22:53 +0600

    Leetcode 220 solution. *RB tree

--- 220/220/220/Source.cpp
+++ 220/220/220/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -8,36 +9,33 @@ using namespace std;
 
 class Solution 
 {
-private:
-	struct bst_node
-	{
-		bst_node* left;
-		bst_node* right;
-
-		int val;
-	};
-
 public:
-
-	void insert_node(bst_node* root, int value)
-	{
-
-	}
-
-	void delete_node(bst_node* root, int value)
-	{
-
-	}
-
-
-	bool search_range(bst_node* root, int value)
-	{
-
-	}
-
 	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) 
 	{
+		if (nums.size() <= 0)
+			return false;
+		set<long long> s;
 
+		s.insert((long long)nums[0]);
+
+		for (int i = 0, j = 1; j < nums.size(); j++)
+		{
+			if (j - i > k)
+			{
+				s.erase((long long)nums[i]);
+				i++;
+			}
+
+			set<long long>::iterator itlow1 = s.lower_bound((long long)t - (long long)nums[j]);
+			set<long long>::iterator itlow2 = s.lower_bound((long long)nums[j] - (long long)t);
+
+			if ((itlow1 != s.end() && abs(*(itlow1)-nums[j]) <= t) || (itlow2 != s.end() && abs(*(itlow2)-nums[j]) <= t))
+				return true;
+
+			s.insert((long long)nums[j]);
+		}
+
+		return false;
 	}
 };
 
