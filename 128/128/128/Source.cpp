b6b7581a76ad8a31f49fd43d85d8abd20a937af6
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-30 17:39:41 +0600

    Leetcode 128 solution. * Union find.

--- 128/128/128/Source.cpp
+++ 128/128/128/Source.cpp
@@ -5,29 +5,86 @@
 
 using namespace std;
 
+
+
 class Solution 
 {
+private:
+	struct element
+	{
+		int value;
+		int rank;
+		int total_element;
+		element* parent;
+
+		element(int val) : value(val), parent(this), rank(0), total_element(1)
+		{
+		}
+	};
 public:
+
+	element* make_set(int value)
+	{
+		element* elem = new element(value);
+		return elem;
+	}
+
+	element* find_set(element* x)
+	{
+		if (x->parent != x)
+			x->parent = find_set(x->parent);
+
+		return x->parent;
+	}
+
+	int union_set(element* x, element* y)
+	{
+		return link_set(find_set(x), find_set(y));
+	}
+
+	int link_set(element* x, element* y)
+	{
+		if (x == y)
+			return x->total_element;
+		if (x->rank > y->rank)
+		{
+			y->parent = x;
+			x->total_element += y->total_element;
+			return x->total_element;
+		}
+		else
+		{
+			x->parent = y;
+			y->total_element += x->total_element;
+
+			if (x->rank == y->rank)
+				y->rank++;
+
+			return y->total_element;
+		}
+	}
+
 	int longestConsecutive(vector<int>& nums) 
 	{
-		int length = 0;
-		unordered_map<int, bool> map;
+		if (nums.size() <= 0)
+			return 0;
+
+		int length = 1;
+		unordered_map<int, element*> map;
 
 		for (int i = 0; i < nums.size(); i++)
 		{
-			if (map[nums[i]] == true)
-				continue;
+			if (map[nums[i]] == NULL)
+				map[nums[i]] = make_set(nums[i]);
 
-			map[nums[i]] = true;
+			if(map[nums[i] - 1] != NULL)
+				length = max(length, union_set(map[nums[i]], map[nums[i] - 1]));
 
-			if (map[nums[i] - 1] == true)
-				length++;
-
-			if (map[nums[i] + 1] == true)
-				length++;
+			if (map[nums[i] + 1] != NULL)
+				length = max(length, union_set(map[nums[i]], map[nums[i] + 1]));
 		}
 
-		return ++length;
+		return length;
 	}
 };
 
