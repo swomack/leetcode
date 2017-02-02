437a2e66da086a1bce1a035f8ca047e02f7f02f9
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-02-02 16:32:07 +0600

    Leetcode 200 solution. * DFS

--- 200/200/200/Source.cpp
+++ 200/200/200/Source.cpp
@@ -4,121 +4,62 @@
 
 using namespace std;
 
-class Solution 
+class Solution
 {
 public:
 
-	struct node
-	{
-		int i, j;
-
-		node* parent;
-		int rank;
-
-		node(int a, int b) : i(a), j(b), parent(this), rank(0)
-		{
-		}
-	};
-
-
-	node* make_set(int i, int j)
-	{
-		node* n = new node(i, j);
-		return n;
-	}
-
-
-	node* find(node* x)
-	{
-		if (x != x->parent)
-			x->parent = find(x->parent);
-
-		return x->parent;
-	}
-
-	bool link(node* a, node* b)
-	{
-		if (a == b)
-			return false;
-
-		if (a->rank > b->rank)
-		{
-			b->parent = a;
-		}
-		else
-		{
-			a->parent = b;
-
-			if (a->rank == b->rank)
-				a->rank++;
-		}
-
-		return true;
-	}
-
-	bool union_set(node* a, node* b)
-	{
-		return link(find(a), find(b));
-	}
-
-	int numIslands(vector<vector<char>>& grid) 
+	int numIslands(vector<vector<char>>& grid)
 	{
 		int islands = 0;
-		
-
 		int row = grid.size();
-
 		if (row == 0)
 			return islands;
 
 		int col = grid[0].size();
-
 		if (col == 0)
 			return islands;
 
-
-		unordered_map<int, node*> sets;
+		vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size()));
 
 		for (int i = 0; i < row; i++)
 		{
 			for (int j = 0; j < col; j++)
 			{
-				if (grid[i][j] == '1')
+				if (grid[i][j] == '1' && visited[i][j] == false)
 				{
-					int key = i * col + j;
-					sets[key] = make_set(i, j);
 					islands++;
-				}
-			}
-		}
-
-		for (int i = 0; i < row; i++)
-		{
-			for (int j = 0; j < col; j++)
-			{
-				if (grid[i][j] == '1')
-				{
-					int fragment_key = i * col + j;
-
-					if (j + 1 < col && grid[i][j + 1] == '1')
-					{
-						int key = i * col + (j + 1);
-						if (union_set(sets[fragment_key], sets[key]))
-							islands--;
-					}
-
-					if (i + 1 < row && grid[i + 1][j] == '1')
-					{
-						int key = (i + 1) * col + j;
-						if (union_set(sets[fragment_key], sets[key]))
-							islands--;
-					}
-
+					visitNeighbours(grid, i, j, visited);
 				}
 			}
 		}
 
 		return islands;
+	}
+
+
+	void visitNeighbours(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited)
+	{
+		visited[i][j] = true;
+
+		if (i > 0 && grid[i - 1][j] == '1' && visited[i - 1][j] == false)
+		{
+			visitNeighbours(grid, i - 1, j, visited);
+		}
+
+		if (j > 0 && grid[i][j - 1] == '1' && visited[i][j - 1] == false)
+		{
+			visitNeighbours(grid, i, j - 1, visited);
+		}
+
+		if (i < grid.size() - 1 && grid[i + 1][j] == '1' && visited[i + 1][j] == false)
+		{
+			visitNeighbours(grid, i + 1, j, visited);
+		}
+
+		if (j < grid[0].size() - 1 && grid[i][j + 1] == '1' && visited[i][j + 1] == false)
+		{
+			visitNeighbours(grid, i, j + 1, visited);
+		}
 	}
 };
 
