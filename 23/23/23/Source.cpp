3a75abbe1635e8888b6cba8c34fafb46e16ba8b6
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-02-02 11:37:26 +0600

    Leetcode 23 solution. * Using merge two

--- 23/23/23/Source.cpp
+++ 23/23/23/Source.cpp
@@ -15,32 +15,57 @@ struct ListNode
 class Solution 
 {
 public:
-	ListNode* mergeKLists(vector<ListNode*>& lists) 
+
+	ListNode* mergeTwo(ListNode* head1, ListNode* head2)
 	{
 		ListNode* head = new ListNode(0);
+		ListNode* tail = head;
 
-		priority_queue<int> q;
-
-		for (int i = 0; i < lists.size(); i++)
+		while (head1 && head2)
 		{
-			while (lists[i])
+			if (head1->val < head2->val)
 			{
-				q.push(lists[i]->val);
-				lists[i] = lists[i]->next;
+				tail->next = head1;
+				head1 = head1->next;
 			}
+			else
+			{
+				tail->next = head2;
+				head2 = head2->next;
+			}
+
+			tail = tail->next;
 		}
 
 
-		while (!q.empty())
+		while (head1)
 		{
-			ListNode* k = new ListNode(q.top());
-			q.pop();
+			tail->next = head1;
+			head1 = head1->next;
+			tail = tail->next;
+		}
 
-			k->next = head->next;
-			head->next = k;
+		while (head2)
+		{
+			tail->next = head2;
+			head2 = head2->next;
+			tail = tail->next;
 		}
 
 		return head->next;
+	}
+
+	ListNode* mergeKLists(vector<ListNode*>& lists) 
+	{
+		if (lists.size() == 0)
+			return NULL;
+		while (lists.size() > 1)
+		{
+			lists[lists.size() - 2] = mergeTwo(lists[lists.size() - 2], lists[lists.size() - 1]);
+			lists.pop_back();
+		}
+
+		return lists[0];
 	}
 };
 
