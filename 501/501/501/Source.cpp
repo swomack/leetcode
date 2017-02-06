7f97936f34f3a245418d1f900a5c86f03a2b49e4
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-02-06 19:19:09 +0600

    * 501 CONTINUED *

---
+++ 501/501/501/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode 
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	vector<int> findMode(TreeNode* root) 
+	{
+
+	}
+};
+
+int main()
+{
+	Solution s;
+	return 0;
+}
