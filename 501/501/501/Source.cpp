9e787a99b1398bb1dad7da73edf90f4007404b35
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-02-06 19:46:22 +0600

    Leetcode 501 solution.

--- 501/501/501/Source.cpp
+++ 501/501/501/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,7 +17,40 @@ class Solution {
 public:
 	vector<int> findMode(TreeNode* root) 
 	{
+		unordered_map<int, int> count;
+		vector<int> result;
 
+		
+		find_mode(root, count);
+
+		int freq = 0;
+		for_each(count.begin(), count.end(), [&result, &freq](pair<int,int> cnt) {
+			if (cnt.second == freq)
+			{
+				result.push_back(cnt.first);
+			}
+			else if (cnt.second > freq)
+			{
+				result.clear();
+				result.push_back(cnt.first);
+				freq = cnt.second;
+			}
+		});
+
+		return result;
+
+	}
+
+
+	void find_mode(TreeNode* root, unordered_map<int, int>& count)
+	{
+		if (root == NULL)
+			return;
+
+		count[root->val]++;
+
+		find_mode(root->left, count);
+		find_mode(root->right, count);
 	}
 };
 
