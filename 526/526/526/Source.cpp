c0868666a02b7c657ace2c800023b06ea6f34ba3
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-04-06 17:47:37 +0600

    *Leetcode 526 solution.

--- 526/526/526/Source.cpp
+++ 526/526/526/Source.cpp
@@ -11,32 +11,15 @@ public:
 	{
 		vector<bool> track(N, false);
 
-		vector<vector<int>> position(16);
-
-		position[1] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-		position[2] = { 1,2,4,6,8,10,12,14 };
-		position[3] = { 1,3,6,9,12,15 };
-		position[4] = { 1,2,4,8,12 };
-		position[5] = { 1,5,10,15 };
-		position[6] = { 1,2,3,6,12 };
-		position[7] = { 1,7,14 };
-		position[8] = { 1,2,4,8 };
-		position[9] = { 1,3,9 };
-		position[10] = { 1,2,5,10 };
-		position[11] = { 1,11 };
-		position[12] = { 1,2,3,4,6,12 };
-		position[13] = { 1,13 };
-		position[14] = { 1,2,7,14 };
-		position[15] = { 1,3,5,15 };
 
 		int result = 0;
 
-		backtrack(N, 1, track, position, result);
+		backtrack(N, 1, track, result);
 
 		return result;
 	}
 
-	void backtrack(int N, int R, vector<bool>& track, vector<vector<int>>& pos, int& result)
+	void backtrack(int N, int R, vector<bool>& track, int& result)
 	{
 		if (R > N)
 		{
@@ -44,13 +27,13 @@ public:
 			return;
 		}
 
-		for (int i = 0; i < pos[R].size() && pos[R][i] <= N; i++)
+		for (int i = 1; i <= N; i++)
 		{
-			if (track[pos[R][i]] == false)
+			if ((i % R == 0 || R % i == 0) && track[i] == false)
 			{
-				track[pos[R][i]] = true;
-				backtrack(N, R + 1, track, pos, result);
-				track[pos[R][i]] = false;
+				track[i] = true;
+				backtrack(N, R + 1, track, result);
+				track[i] = false;
 			}
 		}
 	}
