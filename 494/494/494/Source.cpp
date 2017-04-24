a9a010629b3d190229c4d5c15c918af39ad5741b
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-04-24 18:23:04 +0600

    Leetcode 494 solution. *Improved

--- 494/494/494/Source.cpp
+++ 494/494/494/Source.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> sums;
+
 class Solution 
 {
 public:
 	int findTargetSumWays(vector<int>& nums, int S) 
 	{
+
+		if (nums.size() == 1)
+		{
+			if (abs(nums[0]) == abs(S))
+				return 1;
+			return 0;
+		}
+
+		unordered_map<int, int> map;
+		unordered_map<int, int> map2;
+		int l = nums.size() / 2;
+
+		map.clear();
 		sums.clear();
+
 		sums.push_back(nums[0]);
 		sums.push_back(-nums[0]);
+		fillMap(nums, 1, l, sums, map);
 
-		return findTargetSumWays(nums, S, 1, sums);
+		map2.clear();
+		sums.clear();
+
+		sums.push_back(nums[l]);
+		sums.push_back(-nums[l]);
+		fillMap(nums, l + 1, nums.size(), sums, map2);
+
+		int count = 0;
+
+		for_each(map.begin(), map.end(), [&count, &S, &map2](pair<int, int> element) {
+			
+			count += element.second * map2[S - element.first];
+			
+		});
+
+		return count;
 	}
 
-	int findTargetSumWays(vector<int>& nums, int S, int index, vector<int>& sums)
+	void fillMap(vector<int>& nums, int index, int last_index, vector<int>& sums, unordered_map<int, int>& map)
 	{
-		if (index >= nums.size())
+		if (index >= last_index)
 		{
-			int count = 0;
-
-			for_each(sums.begin(), sums.end(), [&count, &S](int num) {
-				if (num == S)
-					count++;
+			for_each(sums.begin(), sums.end(), [&](int num) {
+				map[num]++;
 			});
-
-			return count;
+			return;
 		}
 
 
@@ -40,12 +68,15 @@ public:
 			sums.push_back(sums[i] - 2 * num);
 		}
 
-		return findTargetSumWays(nums, S, index + 1, sums);
+		return fillMap(nums, index + 1, last_index, sums, map);
 	}
 };
 
 int main()
 {
 	Solution s;
+
+	//int count = s.findTargetSumWays({1,1,1,1,1}, 3);
+
 	return 0;
 }
